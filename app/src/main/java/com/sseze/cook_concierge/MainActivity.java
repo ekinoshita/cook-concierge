72aab0b0c3a455e1cd750eb679f58929a261158f
Eisuke Kinoshita <eisuke.kinoshita@mixi.co.jp>
2016-08-27 17:29:14 +0900

    テキストを入力せずに送信するとトーストが表示される

--- app/src/main/java/com/sseze/cook_concierge/MainActivity.java
+++ app/src/main/java/com/sseze/cook_concierge/MainActivity.java
@@ -1,9 +1,12 @@
 package com.sseze.cook_concierge;
 
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
@@ -15,4 +18,15 @@ public class MainActivity extends AppCompatActivity {
 
     @ViewById(R.id.send_button)
     Button mButton;
+
+    @Click(R.id.send_button)
+    void send() {
+        String textFormStr = mTextForm.getText().toString();
+
+        if (TextUtils.isEmpty(textFormStr)) {
+            Toast.makeText(this, "テキストを入力してください", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, textFormStr, Toast.LENGTH_LONG).show();
+        }
+    }
 }
