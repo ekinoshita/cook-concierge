1b1c60c5d12c8ccf3fcea832dff676830ba48492
Eisuke Kinoshita <eisuke.kinoshita@mixi.co.jp>
2016-08-27 16:55:36 +0900

    Android Annotations で MainActivity を整理

--- app/src/main/java/com/sseze/cook_concierge/MainActivity.java
+++ app/src/main/java/com/sseze/cook_concierge/MainActivity.java
@@ -1,17 +1,18 @@
 package com.sseze.cook_concierge;
 
-import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
-    private EditText mTextForm;
+    @ViewById(R.id.text_form)
+    EditText mTextForm;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+    @ViewById(R.id.send_button)
+    Button mButton;
 }
